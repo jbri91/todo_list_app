4f9188d6a863f2bf761cbbba261d471ed44b4165
Johnny Briones <jbrrii91@gmail.com>
2020-08-09 20:03:32 -0400

    added button

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-// When I click submit, item should be added to screen and saved.
-// When I click delete, it should delete from screen and storage.
-// Item saved should still be present when user comes back to same browser.
-{/* <input type = 'text' class = 'input-list' placeholder = 'What do you want to do?'>
-            <button id='submit-todo' type="button">Enter</button> */} 
-
-
-const button = document.getElementById('submit-todo').addEventListener('click', function(){
-    let key;
-    let value;
-
-    localStorage.setItem(key,value);
-});
+// Things APP Should DO
+// When I click submit, item should be added.
+// When I click delete, it should delete.
+// localstorage (Last thing)
 
 
 
+const submit_todo = document.getElementById('submit_todo');
 
+
+submit_todo.addEventListener('click', function() {
+
+
+})
+
+
+
+
+
+
+
+
+    //     localStorage.setItem(key,value);
+// });
 // This will save the parameters inputted into the user's browser. No where else.
 
 // localStorage.setItem('KeyName1', 5000);
