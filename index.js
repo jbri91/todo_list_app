47fe787b8791bb49308a9ebb0e43909a6df04e9a
Johnny Briones <jbrrii91@gmail.com>
2020-08-11 21:58:52 -0400

    firstElementChild

--- index.js
+++ index.js
@@ -3,26 +3,64 @@
 // When I click delete, it should delete.
 // localstorage (Last thing)
 
+let itemList = document.querySelector('#options');
+
+function fn1() {
+    let str = document.getElementById('textInput').value;
+    itemList.firstElementChild.textContent = str;
+}
 
 
-const submit_todo = document.getElementById('submit_todo');
 
 
-submit_todo.addEventListener('click', function() {
+
+
+
+
+
+
+
+
+
+
+
+// Notes
+
+// let itemList = document.querySelector('#options');
+// console.log(itemList.children[1]);
+// console.log(itemList.childNodes);
+// console.log(itemList.parentNode);
+// console.log(itemList.parentElement);
+// console.log(itemList.firstElementChild);
+// console.log(itemList.firstChild);
+// console.log(itemList.lastChild);
+// console.log(itemList.lastElementChild);
+// console.log(itemList.nextSibling);
+// console.log(itemList.nextElementSibling);
+// console.log(itemList.previousSibling);
+// console.log(itemList.previousElementSibling);
+// itemList.children[1].style.backgroundColor = 'yellow';
+
+
+// Previous button function
+
+// const submit_todo = document.getElementById('submit_todo');
+// submit_todo.addEventListener('click', function() {
     
-})
+// })
 
 
 
+// Create TODO App
+// Input, add to the list.
+// Button, add the list.
+// capture value from input. create a list item with the todo item.
+// Add  attributes to each item. Ex. delete button.
+// Programatically add element to the application.
+// create li from javascript.
+// Functions
+// addItem
+// addTodo
+// create todo. you need to get value of input. set an id on it or a child. text node. That would create the element.
 
 
-
-
-
-    //     localStorage.setItem(key,value);
-// });
-// This will save the parameters inputted into the user's browser. No where else.
-
-// localStorage.setItem('KeyName1', 5000);
-// localStorage.setItem('KeyName2', true);
-// localStorage.setItem('SavedItem' , 'This is saved in local storage');
