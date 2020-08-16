7c22d3a3e224111d516baf9e6cb314987ccb17b8
Johnny Briones <jbrrii91@gmail.com>
2020-08-16 17:14:09 -0400

    array

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const taskKey = "Saved Task";
+
 let li = document.createElement("li");
 
 submit_todo.addEventListener("click", function (e) {
   let deleteButton = document.createElement("button");
   deleteButton.innerHTML = "Delete";
 
-  let str = [JSON.stringify(document.getElementById("textInput").value)];
+  const savedTodos = JSON.parse(localStorage.getItem(taskKey));
   let li = document.createElement("li");
+  let str = JSON.stringify([document.getElementById("textInput").value]);
   li.innerText = str;
   li.appendChild(deleteButton);
   document.getElementById("tasks").appendChild(li);
   localStorage.setItem(taskKey, str);
+
+  savedTodos.push(document.getElementById("textInput").value);
+  localStorage.setItem(taskKey, JSON.stringify(savedTodos));
 
   deleteButton.addEventListener("click", function (e) {
     document.getElementById("tasks").removeChild(li);
@@ -18,20 +23,22 @@ submit_todo.addEventListener("click", function (e) {
   });
 });
 
-
-// const savedTodos = localStorage.getItem(taskKey);
-// console.log(savedTodos);
-// debugger;
 if (taskKey != null) {
-  let deleteButton = document.createElement('button');
+  let deleteButton = document.createElement("button");
   let displayStorage = (li.innerHTML = localStorage.getItem(taskKey));
-  document.getElementById('tasks').appendChild(li);
+  document.getElementById("tasks").appendChild(li);
   li.appendChild(deleteButton);
-  deleteButton.innerHTML = 'Delete';
+  deleteButton.innerHTML = "Delete";
   deleteButton.addEventListener("click", function (e) {
     document.getElementById("tasks").removeChild(li);
-    localStorage.removeItem(taskKey);
-  } 
-  )} else {
-    li.removeChild(deleteButton);
-  }
+    // localStorage.removeItem(taskKey);
+  });
+} else {
+  li.removeChild(deleteButton);
+}
+
+//   const savedTodos = JSON.parse(localStorage.getItem(taskKey));
+
+// savedTodos.push(document.getElementById('textInput'.value));
+
+// localStorage.setItem(taskKey, JSON.stringify(savedTodos));
